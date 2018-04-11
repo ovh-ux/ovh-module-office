a756cc7845509b7799fb2e352c8fcfc60caed920
Marie Jones <marie.jones@corp.ovh.com>
2018-04-11 09:12:49 +0200

    :sparkles: feat(license.alert): Update focus on created alert

--- src/microsoft/office/license/user/microsoft-office-license-user.controller.js
+++ src/microsoft/office/license/user/microsoft-office-license-user.controller.js
@@ -9,9 +9,9 @@ angular.module("Module.microsoft.controllers").controller("MicrosoftOfficeLicens
     }
 
     $onInit () {
-        this.$scope.$on("microsoft.office.license.user.add", () => this.delayedGetUsers());
-        this.$scope.$on("microsoft.office.license.user.edit", () => this.delayedGetUsers());
-        this.$scope.$on("microsoft.office.license.user.delete", () => this.delayedGetUsers());
+        this.$scope.$on("microsoft.office.license.user.add", () => this.refreshUsers());
+        this.$scope.$on("microsoft.office.license.user.edit", () => this.refreshUsers());
+        this.$scope.$on("microsoft.office.license.user.delete", () => this.refreshUsers());
 
         this.getUserIds();
     }
@@ -44,5 +44,14 @@ angular.module("Module.microsoft.controllers").controller("MicrosoftOfficeLicens
 
     delayedGetUsers () {
         return this.$timeout(() => this.getUserIds(), 250);
+    }
+
+    scrollToAlert () {
+        this.$timeout(() => document.getElementById("action-alert").scrollIntoView(false));
+    }
+
+    refreshUsers () {
+        this.delayedGetUsers();
+        this.scrollToAlert();
     }
 });
